ab5d1f87e6cf56baab4e50a4484214fa28bec164
niwatorinoko <rean.ogswr@gmail.com>
2023-07-01 22:42:43 +0900

    2023/7/1

---
+++ Easy/884.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        res = []
+        ss = s1+ " " +s2
+        ss = ss.split()
+        for i in ss:
+            if ss.count(i) == 1:
+                res.append(i)
+        return res
