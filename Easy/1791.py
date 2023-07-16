5be8f57c3d444616b327f721e2cc09b19f0de7a4
niwatorinoko <rean.ogswr@gmail.com>
2023-07-16 22:47:40 +0900

    2023/7/16

---
+++ Easy/1791.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        for i in edges[0]:
+            for j in edges[1]:
+                if i == j:
+                    return i
