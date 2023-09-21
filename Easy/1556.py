c98f44451910448e8128ad7a0c73c701583faeaa
niwatorinoko <rean.ogswr@gmail.com>
2023-09-21 11:29:03 +0800

    2023/9/21

--- Easy/1556.py
+++ Easy/1556.py
@@ -1,4 +1,4 @@
-class Solution:
+"""class Solution:
     def thousandSeparator(self, n: int) -> str:
         n = str(n)
         res = ""
@@ -11,4 +11,25 @@ class Solution:
             else:
                 res = n[-i] + res
             print(i, res)
-        return res
+        return res"""
+
+class Solution:
+    def thousandSeparator(self, n: int) -> str:
+        if n == 0:
+            return "0"
+        ans = ""
+        count = 0
+        while n > 0:
+            print(n,count)
+            if count == 0:
+                ans += str(n%10)
+            elif count%3 == 0:
+                ans += "."
+                ans += str(n%10)
+            else:
+                ans += str(n%10)
+            n = n//10
+            count += 1
+            print(ans)
+            
+        return (ans[::-1])
