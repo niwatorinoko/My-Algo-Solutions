0b5a3f7916bda0342290a190133424b15b6ac3df
niwatorinoko <rean.ogswr@gmail.com>
2023-10-05 00:35:38 +0800

    2023/10/5

--- Other/List.py
+++ Other/List.py
@@ -1,23 +1,6 @@
-library_books = ["プリンセスとデータベース:王国のSQL",
-                 "時をかける寿司:過去のワサビ",
-                 "ゾンビとお茶会:アフタヌーン・アポカリプス",
-                 "宇宙人との結婚式:愛の放射線",
-                 "忍者ペンギン:氷山の下の秘密"]
-'''
-borrowbook = "宇宙人との結婚式:愛の放射線"
-if borrowbook in library_books:
-    library_books.remove(borrowbook)
-    print(borrowbook, "貸し出しました。")
-else:
-    print(borrowbook, "は存在しませんでした。")
-print(library_books)
-'''
-'''
-returnBook = "コーヒー豆の冒険：エスプレッソ宇宙へ"
-library_books.append(returnBook)
-print(returnBook, "が返却されました。")
-print(library_books)
-'''
-print("今現在図書館にある書籍は：")
-for item in library_books:
-    print(item)
+score_list = [51, 79, 83, 26, 98]
+print("生徒5人の成績リスト =",score_list)
+pop_score = score_list[3]
+score_list.pop(3)
+print("削除後の成績リスト =",score_list)
+print("削除した要素 =",pop_score)
