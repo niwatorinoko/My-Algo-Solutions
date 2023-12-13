9a7f8a9fac3040ea08e862085c185c5caa811d2c
niwatorinoko <rean.ogswr@gmail.com>
2023-12-14 02:39:30 +0800

    2023/12/14

---
+++ Easy/2894.py
@@ -0,0 +1,9 @@
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        sum1, sum2 = 0, 0
+        for i in range(1,n+1):
+            if i%m != 0:
+                sum1 += i
+            else:
+                sum2 += i
+        return sum1-sum2
