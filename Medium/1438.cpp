24b17cdf81c16049245faee8d8f03a66f8a2f1ab
niwatorinoko <rean.ogswr@gmail.com>
2023-12-30 21:56:23 +0800

    2023/12/30

---
+++ Medium/1438.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestSubarray(std::vector<int>& nums, int limit) {
+        std::multiset<int> window;
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < nums.size(); ++right) {
+            window.insert(nums[right]);
+
+            while (*window.rbegin() - *window.begin() > limit) {
+                window.erase(window.find(nums[left]));
+                ++left;
+            }
+
+            maxLength = std::max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+};
